78ac45aa5e22879a15753cedccde4d73a80c0923
Marshall Ku <marshall@kakao.com>
2023-05-02 14:16:40 +0900

    Improve readability with local variables

--- send_notification.sh
+++ send_notification.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
 send_discord_notification() {
+    local description="$1"
+    local color="$2"
+    local url="$3"
+    local status="$4"
+
+    local json_payload
+
+    json_payload=$(
+        cat <<EOF
+{
+  "embeds": [
+    {
+      "type": "rich",
+      "title": "Health check failed",
+      "description": "$description",
+      "color": $color,
+      "fields": [
+        {
+          "name": "URL",
+          "value": "$url"
+        },
+        {
+          "name": "Status Code",
+          "value": "$status"
+        }
+      ],
+      "footer": {
+        "text": "$(date)"
+      }
+    }
+  ]
+}
+EOF
+    )
+
     curl \
         -H "Content-Type: application/json" \
-        -d "{ \"embeds\": [{\"type\":\"rich\",\"title\":\"Health check failed\",\"description\":\"$1\",\"color\":$2,\"fields\":[{\"name\":\"URL\",\"value\":\"$3\"},{\"name\":\"Status Code\",\"value\":\"$4\"}],\"footer\":{\"text\":\"$(date)\"}}] }" \
+        -d "$json_payload" \
         "$DISCORD_WEBHOOK_URI"
 }
