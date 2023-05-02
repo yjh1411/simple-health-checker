fb0c117a92e75dbd40f9d7abed5c330f2137ef10
Marshall Ku <marshall@kakao.com>
2023-05-02 15:15:35 +0900

    Add running mode and config files

--- run.sh
+++ run.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-bash src/health_check.sh
+mode='local'
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -M | --mode)
+        mode="$2"
+        shift
+        shift
+        ;;
+    *)
+        echo "Unknown argument: $1"
+        exit 1
+        ;;
+    esac
+done
+
+case "$mode" in
+local)
+    . config.sh
+    export HTTP_STATUS_CHECK
+    HTTP_STATUS_CHECK=$(cat HTTP_STATUS_CHECK)
+    bash src/health_check.sh
+    ;;
+actions)
+    bash src/health_check.sh
+    ;;
+*)
+    echo "Invalid mode: $mode"
+    exit 5
+    ;;
+esac
